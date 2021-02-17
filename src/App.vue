f5c4b9da032af51b2a9cd92927ae5660e435474a
zxjte9411 <a5852241@gmail.com>
2021-02-18 03:18:01 +0800

    write something

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <v-app id="inspire">
+      <NavBar />
+    </v-app>
   </div>
 </template>
+
+<script>
+import NavBar from "@/components/Navbar.vue";
+
+export default {
+  components: {
+    NavBar
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
