e9bdb2a8338f935a72745c5d449604f4b1b79eb9
zxjte9411 <a5852241@gmail.com>
2021-02-19 02:46:15 +0800

    modify router

--- src/router/index.js
+++ src/router/index.js
@@ -6,13 +6,14 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: "/home",
     name: "Home",
     component: Home
   },
   {
-    path: "/about",
-    name: "About"
+    path: "/",
+    name: "root",
+    redirect: { name: "Home" }
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
