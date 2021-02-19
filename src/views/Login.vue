af67dbddc24a70e410a73e7b3801b625d3c2e892
zxjte9411 <a5852241@gmail.com>
2021-02-20 04:21:54 +0800

    add login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,85 @@
 <template>
-  <v-container fluid fill-height> 
-      <v-row>
-          <v-col>
-              
-          </v-col>
-      </v-row>
+  <v-container fill-height fluid class="d-flex align-center mb-10">
+    <v-row class="mt-10 d-flex align-center justify-center">
+      <v-col cols="4">
+        <div class="text-h3 text-left my-3">Login</div>
+        <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+          class="form"
+          @submit.prevent="handleLogin"
+        >
+          <v-text-field
+            v-model="user.email"
+            label="E-mail"
+            :rules="emailRules"
+            outlined
+          ></v-text-field>
+          <v-text-field
+            v-model="user.password"
+            label="Password"
+            :rules="passwordRules"
+            type="password"
+            outlined
+          ></v-text-field>
+          <div v-show="!valid" class="text-left">
+            <span class="red--text text--lighten-1"
+              >E-mail or Password error</span
+            >
+          </div>
+          <v-btn
+            class="login-btn"
+            type="submit"
+            :disabled="isRoading"
+            color="success"
+            >Submit</v-btn
+          >
+        </v-form>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      user: {
+        email: "",
+        password: ""
+      },
+      valid: true,
+      isRoading: false,
+      passwordRules: [
+        v => !!v || "Password is required",
+        v => (v && v.length <= 8) || "password must be more than 8 characters"
+      ],
+      emailRules: [
+        v => !!v || "E-mail is required",
+        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+      ]
+    };
+  },
+  methods: {
+    async handleLogin() {
+      if (this.$refs.form && this.$refs.form.validate()) {
+        this.isRoading = true;
+        await this.$store
+          .dispatch("login", [this.user.email, this.user.password])
+          .then(res => {
+            console.log(res);
+            this.$router.push("/home");
+          })
+          .catch(err => {
+            console.log(err);
+            this.isRoading = false;
+            this.valid = false;
+          });
+      }
+    }
+  }
+};
 </script>
 
 <style></style>
