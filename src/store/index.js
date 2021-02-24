c4a8b3d199331a8a896557f27b91c2641fcc2483
zxjte9411 <a5852241@gmail.com>
2021-02-24 17:35:26 +0800

    add items and cart modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import Login from "./modules/Login/";
+import Login from "./modules/login";
+import Items from "./modules/items";
+import Cart from "./modules/cart";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: { login: Login }
+  modules: { login: Login, items: Items, cart: Cart }
 });
