09b425d353aa5ec0ef09db93c87da1c4fe30e1a6
zxjte9411 <a5852241@gmail.com>
2021-02-24 17:36:00 +0800

    add cart vue

---
+++ src/views/Cart.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fluid fill-height class="d-flex justify-start">
+    <v-row>
+      <v-col> </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
