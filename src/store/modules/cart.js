3df10f35927c37c86203bb4e5b9f85bee844121d
zxjte9411 <a5852241@gmail.com>
2021-02-24 17:36:34 +0800

    add cart and items into store modules

---
+++ src/store/modules/cart.js
@@ -0,0 +1,36 @@
+const getDefaultState = () => {
+  return {
+    // item: [{ id, quantity }]
+    items: [],
+    status: "empty"
+  };
+};
+
+const state = getDefaultState();
+const actions = {
+  resetCartState({ commit }) {
+    commit("resetCartState");
+  }
+};
+
+const mutations = {
+  resetCartState(state) {
+    Object.assign(state, getDefaultState());
+  }
+};
+
+const getters = {
+  Items(state, getters, rootState) {
+    return state.items.map(({ id, quantity }) => {
+      const product = rootState.products.all.find(product => product.id === id);
+      return {
+        title: product.Name,
+        price: product.Price,
+        imageUrl: product.ImageURL,
+        quantity
+      };
+    });
+  }
+};
+
+export default { namespaced: true, state, actions, mutations, getters };
---
+++ src/store/modules/items.js
@@ -0,0 +1,23 @@
+import { GetGamesByCategory } from "../../api/home";
+
+const state = {
+  items: []
+};
+
+const actions = {
+  async getAllGames({ commit }) {
+    const items = (await GetGamesByCategory("all")).data.data;
+    console.log(items);
+    commit("setGames", items);
+  }
+};
+
+const mutations = {
+  setGames(state, items) {
+    state.items = items;
+  }
+};
+
+const getters = {};
+
+export default { namespced: true, state, actions, mutations, getters };
