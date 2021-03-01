eb4fe93498203aa6adf518f7d2d8f01a6fe1d6e0
zxjte9411 <a5852241@gmail.com>
2021-03-01 19:21:22 +0800

    add loading element

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,80 +1,89 @@
 <template>
   <v-container fluid fill-height class="d-flex justify-start">
-    <v-row v-for="i in rowSize" :key="i" class="d-flex justify-start">
-      <v-col
-        v-for="n in getColoumNumber(i)"
-        :key="n"
-        class="d-flex justify-start"
-      >
-        <v-card class="mx-12 my-12" max-width="380" max-height="500">
-          <a
-            ><v-img
-              height="250"
-              :src="getImagePath(games[(i - 1) * 3 + n - 1].ImageURL)"
-              @click="test(games[(i - 1) * 3 + n - 1])"
-          /></a>
-          <v-card-title>{{ games[(i - 1) * 3 + n - 1].Name }}</v-card-title>
-          <v-card-subtitle class="text-start">
-            NT$ {{ games[(i - 1) * 3 + n - 1].Price }}
-          </v-card-subtitle>
-          <v-card-text>
-            <div class="text-left">
-              {{ games[(i - 1) * 3 + n - 1].Description }}
-            </div>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              color="deep-purple lighten-2"
-              text
-              @click="test(games[(i - 1) * 3 + n - 1])"
-            >
-              Add to Cart
-            </v-btn>
-          </v-card-actions>
-        </v-card>
+    <v-row>
+      <v-col cols="8" offset="2">
+        <Loading :is-loading="isLoading" />
       </v-col>
     </v-row>
+    <div v-if="!isLoading">
+      <v-row v-for="i in rowSize" :key="i" class="d-flex justify-start">
+        <v-col
+          v-for="n in getColoumNumber(i)"
+          :key="n"
+          class="d-flex justify-start"
+        >
+          <v-card class="mx-12 my-12" max-width="380" max-height="500">
+            <a
+              ><v-img
+                height="250"
+                :src="getImagePath(games[(i - 1) * 3 + n - 1])"
+            /></a>
+            <v-card-title>{{ games[(i - 1) * 3 + n - 1].Name }}</v-card-title>
+            <v-card-subtitle class="text-start">
+              NT$ {{ games[(i - 1) * 3 + n - 1].Price }}
+            </v-card-subtitle>
+            <v-card-text>
+              <div class="text-left">
+                {{ games[(i - 1) * 3 + n - 1].Description }}
+              </div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="deep-purple lighten-2"
+                text
+                @click="test(games[(i - 1) * 3 + n - 1])"
+              >
+                Add to Cart
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
 import { imgageHost } from "@/config/config";
+import { mapGetters } from "vuex";
+import Loading from "@/components/Loading";
 
 export default {
   name: "Home",
+  components: { Loading },
   data() {
     return {
+      isLoading: true,
       rowSize: 0,
       category: this.$route.query.category
     };
   },
   computed: {
-    games: {
-      get() {
-        return this.$store.state.games.items;
-      },
-      set() {}
-    }
+    ...mapGetters({ games: "games/Items" })
   },
   watch: {
     $route: async function() {
+      this.isLoading = true;
       this.category = this.$route.query.category;
       await this.loadData();
+      this.isLoading = false;
     }
   },
   async created() {
     await this.loadData();
+    this.isLoading = false;
   },
   methods: {
     async loadData() {
       await this.$store.dispatch("games/getAllGames", this.category);
       this.rowSize = this.games.length < 3 ? 1 : this.games.length / 3;
     },
-    getImagePath(filename = "") {
-      return `${imgageHost}/${filename.replace("./img/", "")}`;
+    getImagePath(game) {
+      if (!game) return "https://www.pixiv.net/artworks/86483702";
+      return `${imgageHost}/${game.ImageURL.replace("./img/", "")}`;
     },
     getColoumNumber(index) {
       const length = this.games.length;
