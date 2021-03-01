25bcfb318c049a22bbc110d65d1be20d29dc6685
zxjte9411 <a5852241@gmail.com>
2021-03-01 21:30:26 +0800

    debug

--- src/store/modules/shoppingCart.js
+++ src/store/modules/shoppingCart.js
@@ -70,19 +70,21 @@ const getters = {
     if (rootState.games.items.length <= 0) return [];
     return state.items.map(({ id, quantity }) => {
       const product = rootState.games.items.find(game => game.Game_No === id);
-      return {
-        id: product.Game_No,
-        title: product.Name,
-        price: product.Price,
-        imageUrl: product.ImageURL,
-        quantity
-      };
+      if (product)
+        return {
+          id: product.Game_No,
+          title: product.Name,
+          price: product.Price,
+          imageUrl: product.ImageURL,
+          quantity
+        };
     });
   },
   CartTotalPrice: (state, getters) => {
+    if (getters.CartItems.length === 0) return 0;
     return getters.CartItems.reduce((total, item) => {
       return total + item.price * item.quantity;
-    });
+    }, 0);
   },
   GetItemSubTotal: () => item => {
     return item.price * item.quantity;
