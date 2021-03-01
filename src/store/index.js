1fba01bf2371d6246d3ffc86c163651928f7bac7
zxjte9411 <a5852241@gmail.com>
2021-03-01 16:53:40 +0800

    rename

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import login from "./modules/login";
-import items from "./modules/items";
+import games from "./modules/games";
 import shoppingCart from "./modules/shoppingCart";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: { login, items, shoppingCart }
+  modules: { login, games, shoppingCart }
 });
