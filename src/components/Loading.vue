e2fa68de4182cbb9c31bce0b897ef113d29499da
zxjte9411 <a5852241@gmail.com>
2021-03-01 19:20:47 +0800

    add loading components

---
+++ src/components/Loading.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-sheet v-if="isLoading" color="grey lighten-4" class="pa-3">
+    <v-skeleton-loader
+      class="mx-auto"
+      max-width="300"
+      type="card"
+    ></v-skeleton-loader>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "Loading",
+  props: { isLoading: { type: Boolean, default: true } }
+};
+</script>
+
+<style></style>
