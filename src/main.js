4be0c3fa6f7b534a7d524c96170eb5ed85a26e63
zxjte9411 <a5852241@gmail.com>
2021-03-03 01:43:48 +0800

    add default config for axios

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import store from "./store";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
 import axios from "axios";
+import { host } from "@/config/config";
 
 axios.defaults.withCredentials = true;
+axios.defaults.baseURL = host;
+axios.defaults.headers.common["Content-Type"] = "multipart/form-data";
 
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
