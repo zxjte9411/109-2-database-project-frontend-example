49b5135909bcfd7e149449705a0dfa3c4d6f1619
zxjte9411 <a5852241@gmail.com>
2021-03-07 04:49:26 +0800

    完成購物車相關邏輯

--- src/store/modules/shoppingCart.js
+++ src/store/modules/shoppingCart.js
@@ -1,4 +1,5 @@
-import { GetCoupon } from "@/api/shoopingCart";
+import { GetCoupon, Checkout } from "@/api/shoopingCart";
+import { UpdateWallet } from "@/api/walletOrProfit";
 
 const getDefaultState = () => {
   const cart = localStorage.getItem("cart")
@@ -8,6 +9,7 @@ const getDefaultState = () => {
     // item: [{ id, quantity }]
     items: cart.items,
     coupons: cart.coupons,
+    selectedCoupon: null,
     checkoutStatus: null
   };
 };
@@ -37,7 +39,18 @@ const actions = {
   },
   async GetCoupon({ commit }) {
     const coupones = (await GetCoupon()).data.data;
+    coupones.forEach(coupon => {
+      coupon.Amount = Number(coupon.Amount);
+    });
     commit("SetCoupons", coupones);
+  },
+  async Checkout({ state, commit, getters, rootState, dispatch }) {
+    Checkout(state.items).then(() => {
+      UpdateWallet(rootState.wallet.wallet.value - getters.CartTotalPrice);
+      dispatch("wallet/init", null, { root: true });
+      commit("CleanShoopingCart");
+      commit("resetCartState");
+    });
   }
 };
 
@@ -73,6 +86,13 @@ const mutations = {
   SetCoupons(state, coupons) {
     state.coupons = coupons;
     localStorage.setItem("cart", JSON.stringify(state));
+  },
+  CleanShoopingCart(state) {
+    state.items.length = 0;
+    localStorage.setItem("cart", JSON.stringify(state));
+  },
+  SetSelectedCoupon(state, coupon) {
+    state.selectedCoupon = coupon;
   }
 };
 
@@ -93,9 +113,12 @@ const getters = {
   },
   CartTotalPrice: (state, getters) => {
     if (getters.CartItems.length === 0) return 0;
-    return getters.CartItems.reduce((total, item) => {
-      return total + item.price * item.quantity;
-    }, 0);
+    const discont = state.selectedCoupon ? state.selectedCoupon.Amount : 0;
+    return (
+      getters.CartItems.reduce((total, item) => {
+        return total + item.price * item.quantity;
+      }, 0) - discont
+    );
   },
   GetItemSubTotal: () => item => {
     return item.price * item.quantity;
@@ -105,7 +128,14 @@ const getters = {
       return total + item.quantity;
     }, 0);
   },
-  Coupons: state => state.coupons
+  Coupons: state => state.coupons,
+  SelectedCoupon: state => state.selectedCoupon,
+  IsCouponCanUse: (state, getters) => coupon => {
+    return getters.CartTotalPrice - (coupon ? coupon.Amount : 0) >= 0;
+  },
+  IsCanCheckout: (state, getters, rootState, rootGetters) => {
+    return rootGetters["wallet/GetWallet"].value - getters.CartTotalPrice >= 0;
+  }
 };
 
 export default { namespaced: true, state, actions, mutations, getters };
