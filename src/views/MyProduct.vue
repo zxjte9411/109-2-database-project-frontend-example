ea4ae342d70251abddabb65195220abbd9f534b7
zxjte9411 <a5852241@gmail.com>
2021-03-09 01:12:20 +0800

    add MyProduct view

---
+++ src/views/MyProduct.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fluid fill-height class="d-flex align-start">
+    <v-col>
+      <v-row>
+        <v-col></v-col>
+        <v-col></v-col>
+        <v-col></v-col>
+      </v-row>
+    </v-col>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
